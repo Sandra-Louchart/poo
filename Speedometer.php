f86103dc97dbffdd9a720876fdf6aab3c6ffba47
Sandra-Louchart <sandra.louchart@gmail.com>
2020-11-13 08:51:20 +0100

    POO6

---
+++ Speedometer.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class Speedometer
+{
+    const convertMiles = 0.62;
+    const convertKm = 1.60;
+
+    public static function convertToMiles(int $km)
+    {
+        return $km . 'Km équivaut à '. $km * self::convertMiles . 'miles';
+    }
+
+    public static function convertToKm (int $miles)
+    {
+        return$miles . 'miles éqivaut à '. $miles * self::convertKm . 'Km';
+    }
+}
